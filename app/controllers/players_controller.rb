afaca54baa7a7d041a73970a27f5d5ae4dda8edc
Alcy Torres <mxalcytorres@gmail.com>
2024-05-19 22:01:15 -0400

    Add create action

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -10,5 +10,15 @@ class PlayersController < ApplicationController
     render json: {message: "Jello"}
   end
 
+  def create
+    @player = Player.create(
+      name: params[:name],
+      position: params[:position],
+      nationality: params[:nationality],
+      dob: params[:dob],
+    )
+    render json: {message: "Yello"}
+  end
+
 
 end
