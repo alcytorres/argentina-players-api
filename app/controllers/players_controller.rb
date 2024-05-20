62d20d5c9e24adff26b191e2625d29333e0919d9
Alcy Torres <mxalcytorres@gmail.com>
2024-05-19 21:56:38 -0400

    Add show action

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,6 +5,10 @@ class PlayersController < ApplicationController
     render json: {message: "Hello"}
   end
 
+  def show
+    @player = Player.find_by(id: params[:id])
+    render json: {message: "Jello"}
+  end
 
 
 end
