09cd1c43bfe7f4e158a2aa4497d68ade5936dd78
Alcy Torres <mxalcytorres@gmail.com>
2024-05-19 22:11:13 -0400

    Add destroy action

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -31,5 +31,11 @@ class PlayersController < ApplicationController
     render json: {message: "Zello"}
   end
 
+  def destroy
+    @player = Player.find_by(id: params[:id])
+    @player.destroy
+    render json: { message: "Player destroyed successfully" }
+  end
+
 
 end
